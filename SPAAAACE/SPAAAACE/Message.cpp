8ba0d02dd0d064d39dcdc47bbd12906ac4143fcb
PlasticPack <plasticpack@hotmail.ca>
2016-02-24 08:46:07 -0500

    Message
    Les classes pour les message sont maintenant dispo, il manque les component à charles pour faire un merge complet

---
+++ SPAAAACE/SPAAAACE/Message.cpp
@@ -0,0 +1,48 @@
+#include "Message.h"
+
+using namespace std;
+
+Message::Message()
+{
+	//There's nothing to gain
+}
+Message::~Message()
+{
+	//There's nothing to lose
+}
+
+
+void Message::addMessage(std::string const& sender, std::string const& fromComponent,int const& msFlag ,double const& message)
+{
+	m_messages.insert(std::make_pair(sender + fromComponent,make_pair(msFlag,message)));
+}
+
+std::queue<typeMsPair> Message::getMessageQueue(std::string const& sender, std::string const& fromComponent)
+{
+	pair<typeMessage::iterator, typeMessage::iterator> messagePlage = m_messages.equal_range(sender + fromComponent);
+	queue<typeMsPair> message;
+	for (typeMessage::iterator it = messagePlage.first; it != messagePlage.second; ++it)
+	{
+		message.push(it->second);
+	}
+	return message;
+}
+
+double Message::getMessage(std::string const& sender, std::string const& fromComponent, int const& msFlag)
+{
+	pair<typeMessage::iterator, typeMessage::iterator> messagePlage = m_messages.equal_range(sender + fromComponent);
+	
+	for (typeMessage::iterator it = messagePlage.first; it != messagePlage.second; ++it)
+	{
+		if (it->second.first == msFlag)
+			return it->second.second;
+	}
+
+}
+
+
+void Message::clearAll(void)
+{
+	m_messages.clear();
+}
+
