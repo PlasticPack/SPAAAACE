17e53567fb9f496a5ee776b5cf98863804bbedc9
animorlus <whetley@hotmail.fr>
2016-03-24 19:25:47 -0400

    Revert "Xml"

--- SPAAAACE/SPAAAACE/LuaFunctions.cpp
+++ SPAAAACE/SPAAAACE/LuaFunctions.cpp
@@ -1,13 +1,7 @@
 #include "LuaInit.hpp"
 #include <LuaBridge.h>
-<<<<<<< HEAD
-#include <iostream>
-#include <memory>
-#include"Scene.h"
-=======
->>>>>>> origin
 
-//bool luain::loadScript(lua_State* L, const std::string& filename);
+bool luain::loadScript(lua_State* L, const std::string& filename);
 
 
 void luain::lua_getToStack(lua_State* L, const std::string& variableName){
@@ -77,8 +71,6 @@ std::vector<std::string> luain::getTableKeys(lua_State* L, const std::string& na
 
 	lua_pushnil(L);
 	std::vector<std::string> keys;
-	//Violation of access happeniing right here...
-	//like, just below this line, means the file wa not oppened !
 	while (lua_next(L, -2)) { // get values one by one
 		//std::cout << "*";
 		if (lua_type(L, -1) == LUA_TSTRING) { // check if key is a string
@@ -118,11 +110,7 @@ std::shared_ptr<GameObject> luain::loadGameObjects(Scene *s, lua_State* L, const
 		}
 		else if (componentName == "Graphics"){
 			LuaRef graTable = entityTable[componentName];
-<<<<<<< HEAD
-			addComponent<GraphicsComponent>(obj, graTable);
-=======
 			addComponent<GraphicsComponent>(s, obj, graTable);
->>>>>>> origin
 		}
 		else std::cout << "Unknown component: " << componentName;
 
@@ -136,125 +124,8 @@ std::shared_ptr<GameObject> luain::loadGameObjects(Scene *s, lua_State* L, const
 			obj->get<PhysicsComponent>()->setPositionComp(obj->get<PositionComponent>());
 		}
 	}
-<<<<<<< HEAD
-	if (obj->hasComponent(typeid(PhysicsComponent))){
-		
-	}
-	
-	
-=======
->>>>>>> origin
 	return obj;
 }
 
-void luain::loadFromRep(std::vector<GameObject> &objects, const std::string& filepath, const std::string& ext){
-	std::vector<std::string> files_dir = getFiles(filepath,ext);
-	if (!files_dir.empty()){
-		for (int i = 0; i < files_dir.size(); i++){
-			std::cout << "Loading file :"<<files_dir[i]<<std::endl;
-			lua_State* L = luaL_newstate();
-			luaL_openlibs(L);
-			luaL_dofile(L, std::string(files_dir[i]).c_str());
-			loadGetKeysFunction(L);
-			boost::filesystem::path p(files_dir[i]);
-			std::shared_ptr<GameObject> obj = loadGameObjects(L, p.stem().string());
-			//boost::filesystem::path p(files_dir[i]);
-			obj->setID(p.stem().string());
-			//std::cout << obj->getID() << std::endl;
-			objects.push_back(*obj.get());
-		}
-	}
-	
-}
 
-void luain::loadFromRep(std::map<std::string, GameObject> &objs, const std::string& filepath, const std::string& ext){
-	std::vector<std::string> files_dir = getFiles(filepath, ext);
-	if (!files_dir.empty()){
-		for (int i = 0; i < files_dir.size(); i++){
-			std::cout << "Loading file :" << files_dir[i] << std::endl;
-			lua_State* L = luaL_newstate();
-			luaL_openlibs(L);
-			luaL_dofile(L, std::string(files_dir[i]).c_str());
-			loadGetKeysFunction(L);
-			boost::filesystem::path p(files_dir[i]);
-			std::shared_ptr<GameObject> obj = loadGameObjects(L, p.stem().string());
-			//boost::filesystem::path p(files_dir[i]);
-			if (objs.find(p.stem().string()) == objs.end()){
-				obj->setID(p.stem().string());
-				//std::cout << obj->getID() << std::endl;
-				objs.insert(std::make_pair(obj->getID(), *obj.get()));
-			}
-			else std::cout << "Colision at:" << p.stem().string() << std::endl;
-			
-		}
-	}
-}
 
-std::vector<std::string> getFiles(const std::string& filepath, const std::string& ext){
-	std::vector<std::string> files;
-	//Check if 
-	if (!filepath.empty()){
-		
-		boost::filesystem::path path(filepath);
-		boost::filesystem::recursive_directory_iterator end;
-		for (boost::filesystem::recursive_directory_iterator i(path); i !=end; i++)
-		{
-			//wew, not required at all
-			/*if (boost::filesystem::is_directory(*i)){
-				std::vector<std::string> w = getFiles(boost::filesystem::path(*i).string(), ext);
-				for (int i = 0; i < w.size(); i++)
-				{	
-					files.push_back(w.at(i));
-				}
-			}*/
-			 if (boost::filesystem::is_regular_file(*i) && i->path().extension() == ext){
-				files.push_back(boost::filesystem::path(*i).string());
-			}
-		}
-	}
-	return files;
-}
-
-template <typename T>
-void addComponent(Scene *s, std::shared_ptr<GameObject> e, luabridge::LuaRef& componentTable) {
-	//auto n = T(componentTable);
-
-	e->addComponent(std::type_index(typeid(T)), std::make_shared<T>(componentTable));
-	s->addComponent<T>(e->get<T>());
-}
-
-std::shared_ptr<GameObject> luain::loadGameObjects(Scene *s, lua_State* L, const std::string& type){
-	using namespace luabridge;
-	std::shared_ptr<GameObject> obj = std::make_shared<GameObject>();
-	obj->setID(type);
-
-	auto v = luain::getTableKeys(L, type);
-	LuaRef entityTable = getGlobal(L, type.c_str());
-	for (auto& componentName : v) {
-
-		if (componentName == "Position") {
-			LuaRef poscTable = entityTable[componentName];
-			addComponent<PositionComponent>(s, obj, poscTable);
-		}
-		else if (componentName == "Physics"){
-			LuaRef phycTable = entityTable[componentName];
-			addComponent<PhysicsComponent>(s, obj, phycTable);
-		}
-		else if (componentName == "Graphics"){
-			LuaRef graTable = entityTable[componentName];
-			addComponent<GraphicsComponent>(s, obj, graTable);
-		}
-		else std::cout << "Unknown component: " << componentName;
-
-		std::cout<< "Added " << componentName << " to " << type << std::endl;
-	}
-	if (obj->hasComponent(typeid(PositionComponent))){
-		if (obj->hasComponent(typeid(GraphicsComponent))){
-			obj->get<GraphicsComponent>()->setPositionComponent(obj->get<PositionComponent>());
-		}
-		if (obj->hasComponent(typeid(PhysicsComponent))){
-			obj->get<PhysicsComponent>()->setPositionComp(obj->get<PositionComponent>());
-		}
-	}
-	return obj;
-}
--- SPAAAACE/SPAAAACE/PositionComponent.cpp
+++ SPAAAACE/SPAAAACE/PositionComponent.cpp
@@ -3,7 +3,7 @@
 
 PositionComponent::PositionComponent() : Component()
 {
-	//std::cout << "PCOMP BUILT\n";
+	std::cout << "PCOMP BUILT\n";
 	m_zIndex = 1.0;
 }
 
