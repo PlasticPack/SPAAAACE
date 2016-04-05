090e773fa855c73411819d4cfa461f774bba43f8
shortChangeDev <charles.murphy.042@gmail.com>
2016-04-05 12:21:38 -0400

    Added files via upload

---
+++ SPAAAACE/SPAAAACE/scripts/scene_game/triggers/trigger1.lua
@@ -0,0 +1,14 @@
+trigger1 = {
+	Position = {
+		x = 500,
+		y = 0
+	},
+	Physics = {
+		hitbox = 100,
+		activated = "false"
+	},
+	Action = {
+		trigger = "Scene Player 34048 500.0",
+		answer = "Action Trigger 34061 1"
+	}
+}
