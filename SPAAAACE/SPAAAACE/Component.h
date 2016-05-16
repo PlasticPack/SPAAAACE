acc7cb26171bb84e44945a023a62d5b6d99608e8
shortChangeDev <charles.murphy.042@gmail.com>
2016-05-16 19:04:06 -0400

    Update Component.h

--- SPAAAACE/SPAAAACE/Component.h
+++ SPAAAACE/SPAAAACE/Component.h
@@ -1,3 +1,13 @@
+
+/********************
+Créé par Tristan Roy
+Entité abstraire représentant 
+une partie d'un objet
+Ne contient que des données, 
+qui elles sont manipulées et 
+modifiées par les systèmes
+correspondants
+*********************/
 #pragma once
 
 #include <iostream>
